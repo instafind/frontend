c0bd7d9dddb829bd8b5e14b69926cf7a78be1d89
David Zhu <hdavidzhu@gmail.com>
2016-07-16 01:41:57 -0700

    FIX: Merge conflict.

--- src/panes/search/index.js
+++ src/panes/search/index.js
@@ -1,9 +1,6 @@
 var Vue = require('vue');
+require('./index.scss');
 
 module.exports = Vue.extend({
-  template: require('./index.html'),
-  data: function() {
-    return {
-    }
-  }
+  template: require('./index.html')
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 module.exports = {
   entry: "./src/entry.js",
+  devtool: 'inline-source-map',
   output: {
     path: __dirname,
-    filename: "./dist/bundle.js"
+    filename: "./dist/bundle.js",
   },
   resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.js', '.html']
+    extensions: ['', '.webpack.js', '.web.js', '.js', '.html', '.scss']
   },
   module: {
     loaders: [
-      { test: /\.html$/, loader: 'html-loader' }
+      { test: /\.html$/, loader: 'html-loader' },
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      }
     ]
-  }
+  },
 };
