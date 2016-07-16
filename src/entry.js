4fc456680cdc3d89adc495467711a638bc922941
David Zhu <hdavidzhu@gmail.com>
2016-07-16 02:09:05 -0700

    ADD: Routing capabilities.

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,9 @@
 "use strict";
 
 var Vue = require('vue');
+var VueRouter = require('vue-router');
+Vue.use(VueRouter);
+var App = Vue.extend({});
 
 // Components.
 
@@ -12,5 +15,16 @@ Vue.component('pane-search', PaneSearch);
 var PaneHome = require('./panes/home');
 Vue.component('pane-home', PaneHome);
 
+// Routes.
+var router = new VueRouter();
+router.map({
+  '/search': {
+    component: PaneSearch
+  },
+  '/home': {
+    component: PaneHome
+  }
+})
+
 // Create a new root instance.
-new Vue({ el: '#app' });
+router.start(App, '#app');
